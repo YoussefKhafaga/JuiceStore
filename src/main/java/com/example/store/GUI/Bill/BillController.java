0d902e4767b0ca408c6cd1976c99b92d03e5c16d
YoussefKhafaga <ykhfaga@gmail.com>
2023-12-31 06:03:44 +0200

    Adding statistical analysis

---
+++ src/main/java/com/example/store/GUI/Bill/BillController.java
@@ -0,0 +1,31 @@
+package com.example.store.GUI.Bill;
+
+import com.example.store.Product.Products;
+
+public class BillController {
+    /*@FXML
+    private ImageView logoImage;
+
+    @FXML
+    private Label orderId;
+
+    @FXML
+    private Label dateAndTime;
+
+    @FXML
+    private TableView<Product> tableView;  // Make sure to replace 'Product' with your actual item type
+
+    @FXML
+    private Label totalPrice;
+
+    @FXML
+    private Label paid;
+
+    @FXML
+    private Label remaining;
+    public void initialize(Integer id, String dateTime, TableView<Products> productsTableView
+    , Double total, Double paid, Double remaining) {
+
+    }
+*/
+}
